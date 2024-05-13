5597b3c734bf3ddeb165f6efe00f9dc55c369595
wuarty <leonbrasilino@gmail.com>
2024-05-13 10:05:39 -0300

    Adicionando JavaScript e corrigindo css

---
+++ script.js
@@ -0,0 +1,21 @@
+const apikey = "d088e828778b4fb413c10224d3a2f061";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+  var data = await response.json();
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
